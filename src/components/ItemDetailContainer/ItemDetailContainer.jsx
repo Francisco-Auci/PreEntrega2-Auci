8766c0ea832be87edc5029569a712f48172b4e7f
Francisco Auci <auci.francisco12@gmail.com>
2023-11-15 21:18:39 -0300

    ecommerce

--- src/components/ItemDetailContainer/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer/ItemDetailContainer.jsx
@@ -1,7 +1,7 @@
-import {Input} from "../../../../clase10-evt/Input"
-import Intercambiabilidad from "../../../../clase10-evt/Intercambiabilidad"
-import Caso1 from "../../../../clase10-evt/Caso1"
-import {ViewPort} from "../../../../clase10-evt/ViewPort"
+import {Input} from "../../../clase10/Input"
+import Intercambiabilidad from "../../../clase10/Intercambiabilidad"
+import Caso1 from "../../../clase10/Caso1"
+import {ViewPort} from "../../../clase10/ViewPort"
 import { ItemDetail } from "./ItemDetail/ItemDetail"
 
 export const ItemDetailContainer = () => {
--- src/helpers/mFetch.js
+++ src/helpers/mFetch.js
@@ -1,9 +1,9 @@
 const products = [
-    {id: '1', name: 'Producto 1', category: 'remera', price: '1000', stock: 50, decripcion: 'Lorem'},   
-    {id: '2', name: 'Producto 2', category: 'gorra', price: '1000', stock:  50, decripcion: 'Lorem'},
-    {id: '3', name: 'Producto 3', category: 'gorra', price: '1000', stock: 50, decripcion: 'Lorem'},
-    {id: '4', name: 'Producto 4', category: 'gorra', price: '1000', stock: 50, decripcion: 'Lorem'},
-    {id: '5', name: 'Producto 5', category: 'gorra', price: '1000', stock: 50, decripcion: 'Lorem'}
+    {id: '1', name: 'Producto 1', category: 'remera', price: '1000', stock: 50, decripcion: 'Lorem', imageUrl:'https://www.google.com/imgres?imgurl=https%3A%2F%2Funiversoventura.vteximg.com.br%2Farquivos%2Fids%2F193028-600-600%2FRemera-Manga-Corta-DC-Shoes-Round-We-Go-Urbano-Hombre-Negro-1222102104.jpg%3Fv%3D637789732032600000&tbnid=tNbU8DxD7kezIM&vet=12ahUKEwiXp-KznseCAxVYopUCHXW5Be8QMygDegUIARDWAQ..i&imgrefurl=https%3A%2F%2Fwww.universoaventura.com.ar%2Fremera-manga-corta-dc-shoes-round-we-go-urbano-hombre-negro-1222102104%2Fp&docid=OsPZLV8JOc2EKM&w=600&h=600&q=remeras&ved=2ahUKEwiXp-KznseCAxVYopUCHXW5Be8QMygDegUIARDWAQ'},   
+    {id: '2', name: 'Producto 2', category: 'remera', price: '1000', stock:  50, decripcion: 'Lorem', imageUrl:'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.whiskyndust.com%2Fmujer%2Fremeras-rock-mujer%2F&psig=AOvVaw0yHGbWxsg0ZjCOP2Av0SQi&ust=1700180056339000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCNDEqLSex4IDFQAAAAAdAAAAABAG'},
+    {id: '3', name: 'Producto 3', category: 'gorra', price: '1000', stock: 50, decripcion: 'Lorem', imageUrl:'https://www.google.com/url?sa=i&url=https%3A%2F%2Fnewera.com.ar%2Fgorra-new-era-new-york-yankees-59fifty-q421-whe-blk-12891479.html&psig=AOvVaw2KRsGiu6dmlSu5XWKAOWXO&ust=1700180133729000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCKCxmNmex4IDFQAAAAAdAAAAABAG'},
+    {id: '4', name: 'Producto 4', category: 'gorra', price: '1000', stock: 50, decripcion: 'Lorem', imageUrl:'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.innovasport.com%2Fcaballeros%2Fbeisbol%2Faccesorios%2Fnew-era%2Fgorra-new-era-59fifty-new-york-yankees-mlb-olivo%2Fp%2F000000000000213277&psig=AOvVaw2KRsGiu6dmlSu5XWKAOWXO&ust=1700180133729000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCKCxmNmex4IDFQAAAAAdAAAAABAL'},
+    {id: '5', name: 'Producto 5', category: 'gorra', price: '1000', stock: 50, decripcion: 'Lorem', imageUrl:'https://www.google.com/url?sa=i&url=https%3A%2F%2Fnewera.com.ar%2Fgorra-new-era-new-york-yankees-59fifty-acperf-70331909.html&psig=AOvVaw2KRsGiu6dmlSu5XWKAOWXO&ust=1700180133729000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCKCxmNmex4IDFQAAAAAdAAAAABAR'}
 ]
 
 export const mFetch = (id) => new Promised((res, rej)=>{
